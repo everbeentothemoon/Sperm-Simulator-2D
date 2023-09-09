47deb7b67fb1f285f8035e1ce9222b4b6b3be0ba
everbeentothemoon <NostalgicWRLDWIDE@gmail.com>
2023-09-09 13:44:38 +0200

    foreally

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform target;
+    public float maxSpeed = 10f;
+    public float acceleration = 2f;
+
+    private Vector3 currentVelocity;
+    private bool reachedMaxSpeed = false;
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Debug.LogError("Target not assigned!");
+            return;
+        }
+
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        float desiredSpeed = Mathf.Clamp(currentVelocity.magnitude + acceleration * Time.deltaTime, 0f, maxSpeed);
+
+        currentVelocity = direction * desiredSpeed;
+
+        transform.Translate(currentVelocity * Time.deltaTime);
+
+        if (!reachedMaxSpeed && currentVelocity.magnitude >= maxSpeed)
+        {
+            reachedMaxSpeed = true;
+        }
+    }
+}
